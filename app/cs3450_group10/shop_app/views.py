94585b4bb18150af4579b375ea2bb938f94df758
noahjknight <knight.noah99@gmail.com>
2022-09-29 13:11:01 -0600

    Finished simple navigation prototype

--- app/cs3450_group10/shop_app/views.py
+++ app/cs3450_group10/shop_app/views.py
@@ -6,19 +6,61 @@ from django.http import HttpResponse
 def home_general(request):
     template = loader.get_template('home_general.html')
     return HttpResponse(template.render())
-    
-def logon(request):
-    template = loader.get_template('logon_page.html')
-    return HttpResponse(template.render())
 
-def account(request):
-    template = loader.get_template('account.html')
-    return HttpResponse(template.render())
-
-def menu(request):
-    template = loader.get_template('menu.html')
-    return HttpResponse(template.render())
 
 def home_customer(request):
     template = loader.get_template('home_customer.html')
+    return HttpResponse(template.render())
+
+
+def home_employee(request):
+    template = loader.get_template('home_employee.html')
+    return HttpResponse(template.render())
+    
+def home_manager(request):
+    template = loader.get_template('home_manager.html')
+    return HttpResponse(template.render())
+
+def logon_page(request):
+    template = loader.get_template('logon_page.html')
+    return HttpResponse(template.render())
+
+def account_customer(request):
+    template = loader.get_template('account_customer.html')
+    return HttpResponse(template.render())
+
+def account_employee(request):
+    template = loader.get_template('account_employee.html')
+    return HttpResponse(template.render())
+
+def account_manager(request):
+    template = loader.get_template('account_manager.html')
+    return HttpResponse(template.render())
+
+def menu_customer(request):
+    template = loader.get_template('menu_customer.html')
+    return HttpResponse(template.render())
+
+def menu_employee(request):
+    template = loader.get_template('menu_employee.html')
+    return HttpResponse(template.render())
+
+def menu_manager(request):
+    template = loader.get_template('menu_manager.html')
+    return HttpResponse(template.render())
+
+def cashier_employee(request):
+    template = loader.get_template('cashier_employee.html')
+    return HttpResponse(template.render())
+
+def cashier_manager(request):
+    template = loader.get_template('cashier_manager.html')
+    return HttpResponse(template.render())
+
+def stock_manager(request):
+    template = loader.get_template('stock_manager.html')
+    return HttpResponse(template.render())
+
+def pay_page_manager(request):
+    template = loader.get_template('pay_page_manager.html')
     return HttpResponse(template.render())
