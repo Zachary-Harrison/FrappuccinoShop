0a92443789ed4772fe91ce6d9aa43799a929838c
BaconPopCap <zachary.harrison@aggiemail.usu.edu>
2022-10-08 16:59:36 -0600

    Merged models and added Order() and Store()

--- app/cs3450_group10/shop_app/models.py
+++ app/cs3450_group10/shop_app/models.py
@@ -2,35 +2,54 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 
-# Password encryption is handled when a Customer/Employee/Manager instance is created
-# Passwords stored in the models are the returned hash from Django's password encryption capabilities. A user's actual password is not stored in the database.
+# Password encryption is handled when a Customer/Employee/Manager instance is created Passwords stored in the models
+# are the returned hash from Django's password encryption capabilities. A user's actual password is not stored in the
+# database.
 
-class Customer(models.Model):
+class User(models.Model):
+    # Use Django's supported version of an Enum
+    class Type(models.TextChoices):
+        CUSTOMER = 'Customer'
+        EMPLOYEE = 'Employee'
+        MANAGER = 'Manager'
+
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=14, decimal_places=2)
+    user_type = models.CharField(
+        max_length=8,
+        choices=Type.choices,
+        default=Type.CUSTOMER,
+    )
 
     def __str__(self):
         return self.username
 
+    def get_user_type(self):
+        return self.UserType(self.user_type)
 
-class Employee(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=14, decimal_places=2)
-    hours = models.IntegerField()
+    def makeDrinkFor(self, store, menu, drink, customer):
+        # make sure drink exists
+        if menu.drinkExists(drink):
+            customer.balance -= drink.price
+            store.changeInventory(drink.ingredients)
+        else:
+            print(drink.name, "not found")
+            raise RuntimeError
+        return
 
-    def __str__(self):
-        return self.username
+    def addHours(self, hours):
+        self.hours += hours
 
+    def fireEmployee(self, employee):
+        # managers simply demote employees to customers, not remove them from the database
+        if employee.user_type == User.Type.EMPLOYEE:
+            employee.user_type = User.Type.CUSTOMER
+        else:
+            return
 
-class Manager(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=14, decimal_places=2)
-
-    def __str__(self):
-        return self.username
+    def changeInventory(self, store, drink):
+        store.changeInventory(drink.ingredients)
 
 
 class Ingredient(models.Model):
@@ -52,3 +71,30 @@ class Drink(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    drinks = models.QuerySet(model=Drink)
+
+    def priceOf(self):
+        totalPrice = 0
+        for drink in drinks:
+            totalPrice += drink.price
+        return totalPrice
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    users = models.QuerySet(model=User)
+    menu = models.QuerySet(model=Drink)
+    inventory = models.QuerySet(model=Ingredient)
+
+    def __str__(self):
+        return self.name
+
+    def getMenu(self):
+        return menu
+
+    # TODO: implement this method
+    def changeInventory(self):
+        return
