f9f005d1b44c847ef8c396ef9fcbd4c83a58e375
thatsmellything <thesmelliestman@gmail.com>
2022-10-10 12:49:21 -0600

    removed email requirement for registration

--- app/cs3450_group10/shop_app/forms.py
+++ app/cs3450_group10/shop_app/forms.py
@@ -12,5 +12,5 @@ from django.contrib.auth.models import User
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
 
