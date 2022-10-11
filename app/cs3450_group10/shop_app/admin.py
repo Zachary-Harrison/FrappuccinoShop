bebf6ee4227bb838a8e29519c7ea7fca063c1824
BaconPopCap <zachary.harrison@aggiemail.usu.edu>
2022-10-11 10:19:55 -0600

    Migrated the database and fixed admin.py to register Account instead of User

--- app/cs3450_group10/shop_app/admin.py
+++ app/cs3450_group10/shop_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Ingredient, Drink, Order, Store
+from .models import Account, Ingredient, Drink, Order, Store
 
-admin.site.register(User)
+admin.site.register(Account)
 admin.site.register(Ingredient)
 admin.site.register(Drink)
 admin.site.register(Order)
