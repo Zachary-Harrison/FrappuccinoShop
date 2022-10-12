6c96456903bb8f439c5b9a6ad65e7d0b29f740dc
thatsmellything <thesmelliestman@gmail.com>
2022-10-12 16:18:42 -0600

    tried fixing unit test

--- app/cs3450_group10/cs3450_group10/unit_tests/create_user_unit_test.py
+++ app/cs3450_group10/cs3450_group10/unit_tests/create_user_unit_test.py
@@ -15,16 +15,33 @@ def generate_csrf_token():
 
 
 url = 'http://127.0.0.1:3000/shop_app/register'
-payload = {
-    'csrfmiddlewaretoken': generate_csrf_token(),
-    'username': 'UnitTestUser',
-    'password1': 'thisIsaNewPasswordTest!1',
-    'password2': 'thisIsaNewPasswordTest!1',
-    'Create+User': 'Submit+Query'
-}
+#payload = {
+#    'csrfmiddlewaretoken': generate_csrf_token(),
+#    'username': 'UnitTestUser',
+#    'password1': 'thisIsaNewPasswordTest!1',
+#    'password2': 'thisIsaNewPasswordTest!1',
+#    'Create+User': 'Submit+Query'
+#}
 
-print(requests.get(url))
 
-x = requests.post(url, payload)
-print(x)
+client = requests.session()
+
+client.get(url)
+session = requests.Session()
+response = session.get(url)
+cookiesDict = session.cookies.get_dict()
+print(cookiesDict)
+
+csrftoken = cookiesDict['csrftoken']
+print(csrftoken)
+
+# Pass CSRF token both in login parameters (csrfmiddlewaretoken)
+# and in the session cookies (csrf in client.cookies)
+login_data = dict( csrfmiddlewaretoken=csrftoken, username='userUnitTest', password1='UserTestUnit456',password2='UserTestUnit456', next='/')
+r = client.post(url, data=login_data, headers=dict(Referer=url))
+
+#print(requests.get(url))
+
+x = requests.post(url, login_data)
+print(r)
 #print(generate_csrf_token())
