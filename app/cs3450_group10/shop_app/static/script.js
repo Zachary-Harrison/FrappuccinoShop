ea96c7cda5f40fea67c20e9d37594d9e84b328d7
noahjknight <knight.noah99@gmail.com>
2022-10-27 10:58:25 -0600

    Added basic UI for drink order page

--- app/cs3450_group10/shop_app/static/script.js
+++ app/cs3450_group10/shop_app/static/script.js
@@ -19,6 +19,7 @@ function aboutNav() {
     document.getElementById('home_div').style.display = 'block'
     document.getElementById('account_div').style.display = 'none'
     document.getElementById('menu_div').style.display = 'none'
+    document.getElementById('drink_div').style.display = 'none'
     if(document.getElementById('cashier_div')) {
         document.getElementById('cashier_div').style.display = 'none'
     }
@@ -49,6 +50,7 @@ function settingsNav() {
     document.getElementById('account_div').style.display = 'block'
     document.getElementById('home_div').style.display = 'none'
     document.getElementById('menu_div').style.display = 'none'
+    document.getElementById('drink_div').style.display = 'none'
     if(document.getElementById('cashier_div')) {
         document.getElementById('cashier_div').style.display = 'none'
     }
@@ -79,6 +81,7 @@ function menuNav() {
     document.getElementById('account_div').style.display = 'none'
     document.getElementById('home_div').style.display = 'none'
     document.getElementById('menu_div').style.display = 'block'
+    document.getElementById('drink_div').style.display = 'none'
     if(document.getElementById('cashier_div')) {
         document.getElementById('cashier_div').style.display = 'none'
     }
@@ -109,6 +112,7 @@ function cashierNav() {
     document.getElementById('account_div').style.display = 'none'
     document.getElementById('home_div').style.display = 'none'
     document.getElementById('menu_div').style.display = 'none'
+    document.getElementById('drink_div').style.display = 'none'
     document.getElementById('cashier_div').style.display = 'block'
     if(document.getElementById('stock_div')) {
         document.getElementById('stock_div').style.display = 'none'
@@ -134,6 +138,7 @@ function payNav() {
     document.getElementById('account_div').style.display = 'none'
     document.getElementById('home_div').style.display = 'none'
     document.getElementById('menu_div').style.display = 'none'
+    document.getElementById('drink_div').style.display = 'none'
     document.getElementById('cashier_div').style.display = 'none'
     document.getElementById('stock_div').style.display = 'none'
     document.getElementById('pay_div').style.display = 'block'
@@ -157,7 +162,39 @@ function stockNav() {
     document.getElementById('account_div').style.display = 'none'
     document.getElementById('home_div').style.display = 'none'
     document.getElementById('menu_div').style.display = 'none'
+    document.getElementById('drink_div').style.display = 'none'
     document.getElementById('cashier_div').style.display = 'none'
     document.getElementById('stock_div').style.display = 'block'
     document.getElementById('pay_div').style.display = 'none'
+}
+
+function drinkPageNav() {
+    document.getElementById("about_button").style.backgroundColor = "#333"
+    document.getElementById("about_button").style.borderColor = "#333"
+    document.getElementById("settings_button").style.backgroundColor = "#333"
+    document.getElementById("settings_button").style.borderColor = "#333"
+    document.getElementById("menu_button").style.backgroundColor = "#333"
+    document.getElementById("menu_button").style.borderColor = "#333"
+    if (document.getElementById('cashier_button')) {
+        document.getElementById("cashier_button").style.backgroundColor = "#333"
+        document.getElementById("cashier_button").style.borderColor = "#333"
+    }
+    if (document.getElementById("stock_button")) {
+        document.getElementById("stock_button").style.backgroundColor = "#333"
+        document.getElementById("stock_button").style.borderColor = "#333"
+        document.getElementById("pay_nav_button").style.backgroundColor = "#333"
+        document.getElementById("pay_nav_button").style.borderColor = "#333"
+    }
+
+    document.getElementById('account_div').style.display = 'none'
+    document.getElementById('home_div').style.display = 'none'
+    document.getElementById('menu_div').style.display = 'none'
+    document.getElementById('drink_div').style.display = 'block'
+    if(document.getElementById('cashier_div')) {
+        document.getElementById('cashier_div').style.display = 'none'
+    }
+    if(document.getElementById('stock_div')) {
+        document.getElementById('stock_div').style.display = 'none'
+        document.getElementById('pay_div').style.display = 'none'
+    }
 }
