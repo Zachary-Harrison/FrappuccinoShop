121f93412abd1cea0cbb926be1bb9b1a20f3d986
keldon-boehmer <keldon_boehmer@yahoo.com>
2022-11-04 17:56:23 -0600

    Deleted unused methods of Account model, deleted Store model

--- app/cs3450_group10/shop_app/admin.py
+++ app/cs3450_group10/shop_app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Account, Ingredient, Drink, Order, Store
+from .models import Account, Ingredient, Drink, Order
 
 admin.site.register(Account)
 admin.site.register(Ingredient)
 admin.site.register(Drink)
 admin.site.register(Order)
-admin.site.register(Store)
